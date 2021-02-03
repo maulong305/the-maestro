16c694c196fc7ba343288d96ace1f129c8653142
long <maulong305@gmail.com>
2021-02-03 22:42:04 +0700

    play song and JWT

--- the-maestro-BE/src/main/java/com/nml/themaestro/model/UserPrinciple.java
+++ the-maestro-BE/src/main/java/com/nml/themaestro/model/UserPrinciple.java
@@ -1,4 +1,103 @@
 package com.nml.themaestro.model;
 
-public class UserPrinciple {
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class UserPrinciple implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String password;
+    private Role role;
+    private UserDetail userDetail;
+    private Collection<? extends GrantedAuthority> roles;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+    public UserPrinciple(Long id, String userName, String password, UserDetail userDetail, Collection<? extends GrantedAuthority> roles) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.userDetail = userDetail;
+        this.roles = roles;
+    }
+    public static UserPrinciple build(User user){
+        List<GrantedAuthority> authorities = user.getRoles().stream().map(role ->
+                new SimpleGrantedAuthority(role.getRole())
+        ).collect(Collectors.toList());
+        return new UserPrinciple(
+                user.getId(),
+                user.getUserName(),
+                user.getPassword(),
+                user.getUserDetail(),
+                authorities
+        );
+    };
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public UserDetail getUserDetail() {
+        return userDetail;
+    }
+    public void setUserDetail(UserDetail userDetail) {
+        this.userDetail = userDetail;
+    }
+    public Collection<? extends GrantedAuthority> getRoles() {
+        return roles;
+    }
+    public void setRoles(Collection<? extends GrantedAuthority> roles) {
+        this.roles = roles;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ the-maestro-BE/src/main/java/com/nml/themaestro/service/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.nml.themaestro.service;
+
+import com.nml.themaestro.model.User;
+import com.nml.themaestro.model.UserPrinciple;
+import com.nml.themaestro.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void remove(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByUserName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return UserPrinciple.build(userRepository.findByUserName(userName));
+    }
+}
